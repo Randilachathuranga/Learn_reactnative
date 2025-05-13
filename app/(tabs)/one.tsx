1ed7b635bd655bc14ee98126add34830f0d76c39
Randilachathuranga <randilachathuranga100@gmail.com>
2025-05-13 20:35:07 +0530

    add

--- app/(tabs)/one.tsx
+++ app/(tabs)/one.tsx
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {Image, View} from 'react-native';
 
-export class one extends Component {
-  render() {
-    return (
-      <View>
-        <Text>one</Text>
-      </View>
-    )
-  }
-}
+import React from 'react';
 
-export default one
+const one = () => {
+  return (
+    // Try removing the `flex: 1` on the parent View.
+    // The parent will not have dimensions, so the children can't expand.
+    // What if you add `height: 300` instead of `flex: 1`?
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+
+      <Image
+  source={{uri: '../../assets/images/partial-react-logo.png'}}
+  style={{width: 40, height: 40}}
+/>
+    </View>
+    
+  );
+};
+
+export default one;
